7097f605f94919a9e70d2705e9dbba9c2ce923ae
Gideon Grinberg <grinberg1165@gmail.com>
2025-02-26 17:08:41 -0500

    Fix script

--- targets/1_get_observability.sh
+++ targets/1_get_observability.sh
@@ -6,6 +6,6 @@ mkdir ./tmp
 mkdir ./tmp/targets
 mkdir ./tmp/pointings/
 
-tail -n +2 targets_ra_dec.csv | split -l 1000 - tmp/targets/part_ 
+tail -n +2 ./data/targets_ra_dec.csv | split -l 1000 - tmp/targets/part_ 
 parallel --bar 'python3 pointer.py {} ./tmp/pointings/{/}' ::: ./tmp/targets/part_*
 curl -d "Finished!" ntfy.sh/pipeline
